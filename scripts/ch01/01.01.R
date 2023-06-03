b31428c17799e464da71a37ec6b678ea2c518e0f
Yusuke_Saito <ysaito8015@gmail.com>
2023-06-03 19:24:03 +0900

    update 01.01

--- scripts/ch01/01.01.R
+++ scripts/ch01/01.01.R
@@ -8,28 +8,39 @@ pacman::p_load(tidyverse)
 # infected rate, range 0.05 - 0.20, mean 0.10
 a <- 2  # a patameter of beta distribution
 b <- 20 # a patameter of beta distribution
+print("theta: infected rate")
+print("Parameter space: Theta = [0,1], theta in Theta")
+print("y: count of infected people in sample")
+print("Sample space: mathcal{Y} = {0,1,...,20}, y in mathcal{Y}")
+print("Y|theta ~ binomial(20, theta)")
+print("the true infection rate is 0.05, then the probability that there will be zero infected infividuals in the sample is 36%")
+sprintf("p({Y=0}|{theta = 0.05}) ~ binom(20, 0.05):         %.5f", dbinom(x = 0, size = 20, prob = 0.05))
+print("the prior distribution: theta ~ beta(2, 20)")
 # mean of theta ~ beta(a, b), expectation of the prior distribution
-a / (a + b) # [1] 0.09090909
+sprintf("Mean of theta ~ beta(2, 20):                       %.5f", a / (a + b)) # [1] 0.09090909
 # mode of theta ~ beta(a, b), mode of the prior distribution
-(a - 1) / ((a - 1) + (b - 1)) # [1] 0.05
+sprintf("Mode of theta ~ beta(2, 20):                       %.5f", (a - 1) / ((a - 1) + (b - 1))) # [1] 0.05
 # p(theta < 0.10), the prior distribution
-pbeta(0.10, a, b) # [1] 0.63527 64%
+sprintf("p(theta < 0.10), theta  ~ beta(2, 20):             %.5f", pbeta(q = 0.10, shape1 = a, shape2 = b)) # [1] 0.63527 64%
 # range of p(0.05 < theta < 0.20), the prior distribution
-pbeta(0.20, a, b) - pbeta(0.05, a, b) # [1] 0.6593258 66%
+sprintf("p(0.05 < theta < 0.20), theta  ~ beta(2, 20):      %.5f", pbeta(0.20, a, b) - pbeta(0.05, a, b)) # [1] 0.6593258 66%
 
 n <- 20  # number of samples
 x <- 0:n # individuals
 y <- 0   # nubmer of infected
 
 # If Y|theta ~ binomial(n, theta) and theta ~ beta(a, b)
-# mean of theta|{Y=0} ~ beta(a, b + n), the posterior distribution
-a / (a + b + n) # [1] 0.04761905
-# mode of theta ~ beta(a, b), the posterior distribution
-(a + y - 1) / ((a - 1) + (b + n - 1)) # [1] 0.025
+print("If Y|theta ~ binomial(n, theta) and theta ~ beta(a, b)")
+print("theta|{Y=y} ~ beta(a + y, b + n - y)")
+print("If y = 0 then theta|{Y=0} ~ beta(2, 40)")
+# mean of theta|{Y=0} ~ beta(a + y, b + n - y), the posterior distribution
+sprintf("Mean of theta|{Y=0} ~ beta(2+0, 20+20-0):          %.5f", a / (a + b + n)) # [1] 0.04761905
+# mode of theta|{Y=0} ~ beta(a + y, b + n - y), the posterior distribution
+sprintf("Mode of theta|{Y=0} ~ beta(2+0, 20+20-0):          %.5f", (a + y - 1) / ((a - 1) + (b + n - 1))) # [1] 0.025
 # p(theta < 0.10|Y=0), the posterior distribution
-pbeta(0.10, a + y, b + n - y) # [1] 0.9260956 93%
+sprintf("p(theta < 0.10|{Y=0}) ~ beta(2+0, 20+20-0):        %.5f", pbeta(0.10, a + y, b + n - y)) # [1] 0.9260956 93%
 # p(0.05 < theta < 0.20|Y=0), the posterior distribution
-pbeta(0.20, a + y, b + n - y) - pbeta(0.05, a + y, b + n - y) # [1] 0.3843402 38%
+sprintf("p(0.05 < theta < 0.20|{Y=0}) ~ beta(2+0, 20+20-0): %.5f", pbeta(0.20, a + y, b + n - y) - pbeta(0.05, a + y, b + n - y)) # [1] 0.3843402 38%
 
 #jpeg(
 pdf(
@@ -46,7 +57,6 @@ pdf(
     par(
         mfrow = c(1, 2)
     )
-    dbinom(0, 20, 0.05)
     del <- 0.25
     plot(
          range(x - del),
